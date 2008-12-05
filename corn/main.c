08a46cbb1537bb70cfceea0cdff75c6a3e44c5ef
Nick Welch <mack@incise.org>
2008-12-05 12:38:57 -0800

    don't free that!

--- corn/main.c
+++ corn/main.c
@@ -95,7 +95,6 @@ int main(int argc, char ** argv)
     /* make sure the config directory exists */
     const gchar * dir = g_build_filename(g_get_user_config_dir(), PACKAGE, NULL);
     mkdir(dir, S_IRWXU|S_IRWXG|S_IRWXO);
-    g_free(dir);
 
     loop = g_main_loop_new(NULL, FALSE);
 
