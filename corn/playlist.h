e09f597bad287273ff70b4de7e62a347993b95fa
Nick Welch <mack@incise.org>
2008-12-09 20:11:51 -0800

    formatting

--- corn/playlist.h
+++ corn/playlist.h
@@ -21,19 +21,19 @@ extern gint playlist_position;
 
 void playlist_init(void);
 void playlist_destroy(void);
-void  playlist_append_single        (const gchar *path);
-void  playlist_append_alternatives  (const gchar *path, gchar *const* alts);
-void  playlist_replace_path         (guint track, const gchar *path);
+void playlist_append_single(const gchar *path);
+void playlist_append_alternatives(const gchar *path, gchar *const* alts);
+void playlist_replace_path(guint track, const gchar *path);
 /* re-create the random ordering */
-void  playlist_rerandomize          (void);
-void  playlist_advance              (gint num, gboolean loop);
-void  playlist_seek                 (gint track);
-void  playlist_clear                (void);
-void  playlist_remove               (gint track);
+void playlist_rerandomize(void);
+void playlist_advance(gint num, gboolean loop);
+void playlist_seek(gint track);
+void playlist_clear(void);
+void playlist_remove(gint track);
 /* before = -1 to move to the end of the list */
-void  playlist_move                 (gint track, gint dest);
-void  playlist_next_alternative     (void);
-void  playlist_dump                 (void);
-gboolean playlist_fail              (void);
+void playlist_move(gint track, gint dest);
+void playlist_next_alternative(void);
+void playlist_dump(void);
+gboolean playlist_fail(void);
 
 #endif
